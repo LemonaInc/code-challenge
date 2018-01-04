ba495cf529052438c72b6f318bdd57ad95dcdde3
LemonaInc <jaxonstevens@lemonainc.com>
2018-01-03 20:34:53 -0800

    Added background image and basic elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  appTitleName = 'Video Player';
+  appDescriptionText = "Welcome to the video player";
+  backButtonText = "Back";
+  playVideoButtonText = "Play Video";
 }
